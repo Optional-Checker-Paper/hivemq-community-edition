79033f34b422b04a0d22d22c7fc9537ea732f823
Michael Ernst <mernst@cs.washington.edu>
2023-12-09 16:33:34 -0800

    Make buildfile consistent with annotated branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -312,8 +312,10 @@ configure<CheckerFrameworkExtension> {
     )
     extraJavacArgs = listOf(
         "-AsuppressWarnings=type.anno.before.modifier,type.anno.before.decl.anno",
-        "-AassumePure"
+        "-AassumePure",
+        "-AwarnUnneededSuppressions"
     )
+    excludeTests = true
 }
 
 tasks.forbiddenApisTest { enabled = false }
